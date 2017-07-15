821ae903d039198b966da1fd34603b5218bf5bcf
drew mcmahon <dmcmahon11@gmail.com>
2017-07-15 05:25:50 +0000

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+    entry: [
+        './src/app.js',
+    ],
+    output: {
+        path: path.resolve('dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
